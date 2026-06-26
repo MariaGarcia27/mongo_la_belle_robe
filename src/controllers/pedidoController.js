3207b678a77891a72d0390667d9014b56d58e7ee
mdcra13 <marialinaramosaranda13@gmail.com>
2026-06-25 22:20:26 -0500

    resolucion de fallo de mantenibilidad

--- src/controllers/pedidoController.js
+++ src/controllers/pedidoController.js
@@ -26,7 +26,7 @@ const createPedido = async (req, res) => {
       }
 
       const producto = await Producto.findById(productoId);
-      if (!producto?.activo) {
+      if (producto?.activo !== true){
         return res.status(404).json({ mensaje: `Producto no encontrado: ${productoId}` });
       }
 
